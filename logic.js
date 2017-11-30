638263e07cd94a9d7e48ba1556da96eccf54bab8
Jared Gross <jagross@udel.edu>
2017-11-29 22:58:44 -0500

    first draft of calculator

--- logic.js
+++ logic.js
@@ -1,6 +1,5 @@
-var calculator = document.querySelector('#calculator');
 var display = document.querySelector('#display');
-var keys = document.querySelectorAll('.keys'), i;
+var keys = document.querySelectorAll('.keys');
 var compute = document.querySelector('.compute')
 var clear = document.querySelector('.clear')
 
@@ -10,19 +9,19 @@ const addToDisplay = [].forEach.call(keys, function(keys){
   })
 })
 
-function toPercentage(){
-  var percent = document.querySelector('.percent')
+
+const percent = document.querySelector('.percent')
   percent.addEventListener('click', function(){
     display.value = eval(display.value)/100
   })
-}
+
 
 const clearDisplay = clear.addEventListener('click', function(){
   display.value = ''
 })
 
 const computeDisplay = compute.addEventListener('click', function(){
-  if((display.value).includes('/0')){
+  if(display.value.includes('/0')){
     display.value = 'ERROR'
   }else{
   display.value = eval(display.value)
